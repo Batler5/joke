8932864d4fa929a28ebd353cdfa442cb8a88c3d5
Batler5 <115727657+Batler5@users.noreply.github.com>
2023-02-26 16:34:31 +0300

    up

--- oop.js
+++ oop.js
@@ -1,5 +1,21 @@
 "use strict";
 
+
+function hi(){
+    console.log('Say hi!');
+    // debugger;
+}
+hi();
+
+const arr = [1,7,14,30,25],
+        sorted = arr.sort(compareNum);
+
+function compareNum(a,b){
+    return a-b;
+}
+
+console.log(arr);
+console.log(sorted);
 // let str = "some";
 // let strObj = new String(str);
 
@@ -8,21 +24,21 @@
 
 // console.dir([1,2,3]);
 
-const soldier = {
-    health: 400,
-    armor: 100,
-    sayHello: function(){
-        console.log('hello !');
-    }
-};
+// const soldier = {
+//     health: 400,
+//     armor: 100,
+//     sayHello: function(){
+//         console.log('hello !');
+//     }
+// };
 
 
 // const John={
 //     health:100
 // };
 
-const John = Object.create(soldier);
+// const John = Object.create(soldier);
 // John.__proto__=soldier;
 // Object.setPrototypeOf(John, soldier);
 
-John.sayHello();
+// John.sayHello();
