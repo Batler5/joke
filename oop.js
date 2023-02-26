47d36657f2d02239baaf22e8d75775df60964387
Daniil <danikshender6@gmail.com>
2023-02-26 18:49:33 +0300

    ad 1

--- oop.js
+++ oop.js
@@ -1,5 +1,31 @@
 "use strict";
 
+
+// console.log(typeof(String(4)));
+
+// const num =5;
+// console.log("https://vk.com/catalog/"+num);
+// const fontSize=26+'px';
+
+// console.log(typeof(Number('4')));
+// console.log(typeof((+'4')));
+// console.log(typeof(parseInt("15px", 10)));
+
+// let answer =+prompt("hello","");
+
+// let switcher;
+// if(switcher){
+//     console.log("true");
+// } else{
+//     console.log("false");
+// }
+
+// console.log(typeof(Boolean(4)));
+
+
+console.log(typeof(!!null));
+
+
 // let str = "some";
 // let strObj = new String(str);
 
@@ -8,21 +34,21 @@
 
 // console.dir([1,2,3]);
 
-const soldier = {
-    health: 400,
-    armor: 100,
-    sayHello: function(){
-        console.log('hello !');
-    }
-};
+// const soldier = {
+//     health: 400,
+//     armor: 100,
+//     sayHello: function(){
+//         console.log('hello !');
+//     }
+// };
 
 
 // const John={
 //     health:100
 // };
 
-const John = Object.create(soldier);
+// const John = Object.create(soldier);
 // John.__proto__=soldier;
 // Object.setPrototypeOf(John, soldier);
 
-John.sayHello();
+// John.sayHello();
